b912c5c496b31672f687692d53dbce866319dbbf
niktorea <nikatushishvili16@gmail.com>
2025-08-13 14:02:12 +0400

    secons

--- level 07/claswork/ddddfdfdfd.py
+++ level 07/claswork/ddddfdfdfd.py
@@ -1,20 +1,6 @@
-
-a = 10
-b = 20
-
-print(a == b)    # False – ტოლია თუ არა: 10 არ უდრის 20-ს
-print(a != b)    # True – არ ტოლია თუ არა: 10 ნამდვილად არ უდრის 20-ს
-print(a > b)     # False – მეტია თუ არა: 10 არ არის მეტი 20-ზე
-print(a < b)     # True – ნაკლებია თუ არა: 10 ნამდვილად ნაკლებია 20-ზე
-print(a >= b)    # False – მეტია ან ტოლია: 10 არც მეტია და არც ტოლია 20-ს
-print(a <= b)    # True – ნაკლებია ან ტოლია: 10 ნაკლებია 20-ზე
-
-
-
 print(True and False or False or True and (False and False) or (True and True) or (False or True) or False or True and False)
 
-
-
+#პასუხი true
 
 
 
---
+++ level 13/claswork/hi.py
@@ -0,0 +1,16 @@
+charcho = 8
+shexedva = int(input("გამოიცანი რიცხვი: "))
+while shexedva != charcho:
+    shexedva = int(input("არასწორია. სცადე ისევ: "))
+print("გილოცავ! სწორად გამოიცანი")
+
+
+
+
+number = int(input("შეიყვანეთ რაიმე რიცხვი: "))
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
